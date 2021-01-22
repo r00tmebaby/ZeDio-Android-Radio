6ed22313e4ba11bd7a2927b531cba3809a1e83a7
Zdravko Georgiev <32423971+r00tmebaby@users.noreply.github.com>
2021-01-22 23:57:16 +0000

    Refactoring media player. Removed external AsyncPlayer class.

--- app/src/main/java/com/r00tme/radiojava/RadioAdapter.java
+++ app/src/main/java/com/r00tme/radiojava/RadioAdapter.java
@@ -2,7 +2,10 @@ package com.r00tme.radiojava;
 
 import android.content.Context;
 import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.net.Uri;
+
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,10 +21,11 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.Priority;
 import com.bumptech.glide.request.RequestOptions;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class RadioAdapter extends  RecyclerView.Adapter<RadioAdapter.ViewHolder>{
-
+    private static final MediaPlayer mediaPlayer = new MediaPlayer();
     private static final String TAG = "RadioViewAdapter";
     private final ArrayList<Radio> radioList;
     private final Context mContext;
@@ -49,14 +53,15 @@ public class RadioAdapter extends  RecyclerView.Adapter<RadioAdapter.ViewHolder>
         holder.radioCountry.setText(currentRadio.getRadioCountry());
         holder.radioViewLayout.setOnClickListener(v -> {
             Toast.makeText(mContext, "Loading: " + currentRadio.getRadioName(), Toast.LENGTH_LONG).show();
-            Radio newRadio = new Radio(
-                    currentRadio.getRadioName(),
-                    currentRadio.getRadioGenre(),
-                    currentRadio.getRadioCountry(),
-                    currentRadio.getRadioUrl(),
-                    currentRadio.getRadioLogo());
-            ActionChains playback = new ActionChains(mContext,newRadio);
-            playback.playRadio();
+            Log.d(TAG, String.valueOf(mediaPlayer.isPlaying()));
+            try {
+                mediaPlayer.reset();
+                mediaPlayer.setDataSource(String.valueOf(Uri.parse(currentRadio.getRadioUrl())));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            mediaPlayer.prepareAsync();
+            mediaPlayer.setOnPreparedListener(MediaPlayer::start);
         });
     }
 
